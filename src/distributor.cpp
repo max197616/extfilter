e0cb8b2dc05e2d6704725e0b1b46b76ab818eee4
root <root@filter.tushino.com>
2017-02-13 14:57:45 +0300

    version 0.3

---
+++ src/distributor.cpp
@@ -0,0 +1,25 @@
+
+#include <Poco/Util/ServerApplication.h>
+
+#include <rte_config.h>
+#include <rte_ethdev.h>
+
+#include "distributor.h"
+
+Distributor::Distributor(unsigned num_workers):
+	_num_workers(num_workers)
+{
+	distr = rte_distributor_create("PKT_DIST", rte_socket_id(), _num_workers);
+	if(!distr)
+	{
+		Poco::Util::Application::instance().logger().fatal("Unable to create rte_distributor");
+		throw Poco::Exception("Unable to create rte_distributor");
+	}
+}
+
+
+Distributor::~Distributor()
+{
+
+}
+
